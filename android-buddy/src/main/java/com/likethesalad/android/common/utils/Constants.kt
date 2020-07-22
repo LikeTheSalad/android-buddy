6dd5c80430c6d388b163b27ac7514d09f77467d9
Cesar <amqcesar@gmail.com>
2020-07-22 18:16:06 +0100

    Added byte buddy version from gradle to kotlin

--- android-buddy/src/main/java/com/likethesalad/android/common/utils/Constants.kt
+++ android-buddy/src/main/java/com/likethesalad/android/common/utils/Constants.kt
@@ -1,8 +1,10 @@
 package com.likethesalad.android.common.utils
 
+import com.likethesalad.android.generated.BuildConfig
+
 object Constants {
     const val LIBRARY_PROPERTIES_DIR = "META-INF/android-buddy-plugins"
     const val PLUGINS_PROPERTIES_FILE_NAME = "plugins.properties"
     const val PLUGINS_PROPERTIES_CLASSES_KEY = "plugin-classes"
-    const val BYTE_BUDDY_DEPENDENCY_VERSION = "1.10.13"
+    const val BYTE_BUDDY_DEPENDENCY_VERSION = BuildConfig.BYTE_BUDDY_VERSION
 }
