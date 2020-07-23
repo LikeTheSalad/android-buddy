41df911c04ad20b7996c82ea76ac394bce12fb98
Cesar <amqcesar@gmail.com>
2020-07-23 14:57:50 +0100

    Updated tests for PluginFactoriesProvider

--- android-buddy-plugin/src/test/java/com/likethesalad/android/buddy/bytebuddy/PluginFactoriesProviderTest.kt
+++ android-buddy-plugin/src/test/java/com/likethesalad/android/buddy/bytebuddy/PluginFactoriesProviderTest.kt
@@ -3,13 +3,18 @@ package com.likethesalad.android.buddy.bytebuddy
 import com.google.common.truth.Truth
 import com.likethesalad.android.buddy.bytebuddy.utils.ByteBuddyClassesInstantiator
 import com.likethesalad.android.buddy.providers.PluginClassNamesProvider
-import com.likethesalad.android.buddy.utils.AndroidBuddyLibraryPluginsExtractor
 import com.likethesalad.android.common.providers.ProjectLoggerProvider
+import com.likethesalad.android.common.providers.impl.DefaultClassGraphFilesProvider
+import com.likethesalad.android.common.utils.ClassGraphProvider
+import com.likethesalad.android.common.utils.ClassGraphProviderFactory
 import com.likethesalad.android.common.utils.InstantiatorWrapper
 import com.likethesalad.android.common.utils.Logger
+import com.likethesalad.android.common.utils.PluginsFinder
+import com.likethesalad.android.common.utils.PluginsFinderFactory
 import com.likethesalad.android.testutils.BaseMockable
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.slot
 import io.mockk.verify
 import net.bytebuddy.build.Plugin
 import net.bytebuddy.description.type.TypeDescription
@@ -31,7 +36,16 @@ class PluginFactoriesProviderTest : BaseMockable() {
     lateinit var byteBuddyClassesInstantiator: ByteBuddyClassesInstantiator
 
     @MockK
-    lateinit var androidBuddyLibraryPluginsExtractor: AndroidBuddyLibraryPluginsExtractor
+    lateinit var pluginsFinderFactory: PluginsFinderFactory
+
+    @MockK
+    lateinit var pluginsFinder: PluginsFinder
+
+    @MockK
+    lateinit var classGraphProviderFactory: ClassGraphProviderFactory
+
+    @MockK
+    lateinit var classGraphProvider: ClassGraphProvider
 
     @MockK
     lateinit var logger: Logger
@@ -58,7 +72,7 @@ class PluginFactoriesProviderTest : BaseMockable() {
         }.returns(loggerArgumentResolver)
         pluginFactoriesProvider = PluginFactoriesProvider(
             pluginClassNamesProvider, instantiatorWrapper, byteBuddyClassesInstantiator,
-            androidBuddyLibraryPluginsExtractor, logger, projectLoggerProvider
+            pluginsFinderFactory, classGraphProviderFactory, logger, projectLoggerProvider
         )
     }
 
@@ -69,17 +83,26 @@ class PluginFactoriesProviderTest : BaseMockable() {
         val name1 = createNameAndPluginAndFactory(SomePlugin1::class.java, classLoader)
         val name2 = createNameAndPluginAndFactory(SomePlugin2::class.java, classLoader)
         val libName1 = createNameAndPluginAndFactory(SomeLibPlugin1::class.java, classLoader)
+        val classGraphFilesProviderCaptor = slot<DefaultClassGraphFilesProvider>()
         every { pluginClassNamesProvider.getPluginClassNames() }.returns(
             setOf(name1.name, name2.name)
         )
         every {
-            androidBuddyLibraryPluginsExtractor.extractPluginNames(jarFiles)
+            classGraphProviderFactory.create(capture(classGraphFilesProviderCaptor))
+        }.returns(classGraphProvider)
+        every {
+            pluginsFinderFactory.create(classGraphProvider)
+        }.returns(pluginsFinder)
+        every {
+            pluginsFinder.findBuiltPluginClassNames()
         }.returns(setOf(libName1.name))
 
         val factories = pluginFactoriesProvider.getFactories(jarFiles, classLoader)
 
         Truth.assertThat(factories)
             .containsExactly(name1.expectedFactory, name2.expectedFactory, libName1.expectedFactory)
+        Truth.assertThat(classGraphFilesProviderCaptor.captured.provideFiles())
+            .containsExactlyElementsIn(jarFiles)
         verify {
             logger.d("Local plugins found: {}", setOf(name1.name, name2.name))
             logger.d("Dependencies plugins found: {}", setOf(libName1.name))
