0568bdb38c7aedf8d1c97691551bf30d613fa770
Cesar <amqcesar@gmail.com>
2020-07-24 15:32:53 +0100

    Updated tests

--- android-buddy-plugin/src/test/java/com/likethesalad/android/buddy/providers/impl/AndroidPluginDataProviderTest.kt
+++ android-buddy-plugin/src/test/java/com/likethesalad/android/buddy/providers/impl/AndroidPluginDataProviderTest.kt
@@ -2,9 +2,12 @@ package com.likethesalad.android.buddy.providers.impl
 
 import com.android.build.gradle.AppExtension
 import com.android.build.gradle.api.ApplicationVariant
+import com.android.builder.model.BuildType
+import com.android.builder.model.ProductFlavor
 import com.google.common.truth.Truth
 import com.likethesalad.android.buddy.providers.AndroidExtensionProvider
 import com.likethesalad.android.buddy.utils.android.AndroidPluginDataProvider
+import com.likethesalad.android.buddy.utils.android.AndroidVariantPathResolver
 import com.likethesalad.android.buddy.utils.android.AndroidVariantPathResolverFactory
 import com.likethesalad.android.common.utils.Logger
 import com.likethesalad.android.testutils.BaseMockable
@@ -16,7 +19,6 @@ import org.gradle.api.DomainObjectSet
 import org.gradle.api.JavaVersion
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.api.tasks.compile.JavaCompile
-import org.junit.Assert.fail
 import org.junit.Before
 import org.junit.Test
 import java.io.File
@@ -62,16 +64,13 @@ class AndroidPluginDataProviderTest : BaseMockable() {
 
     @Test
     fun `Get variant java target compatibility version`() {
-        val appVariants = mockk<DomainObjectSet<ApplicationVariant>>()
         val javaCompileProvider = mockk<TaskProvider<JavaCompile>>()
         val javaCompile = mockk<JavaCompile>()
         val targetCompatibility = "1.7"
-        val variant = createApplicationVariantMock(variantName)
+        val variant = createAndSetMainVariantMock("otherName")
         every { javaCompileProvider.hint(JavaCompile::class).get() }.returns(javaCompile)
-        every { androidAppExtension.applicationVariants }.returns(appVariants)
-        every { appVariants.iterator() }.returns(getVariantsIterator(variant, "otherName"))
-        every { variant.javaCompileProvider }.returns(javaCompileProvider)
         every { javaCompile.targetCompatibility }.returns(targetCompatibility)
+        every { variant.javaCompileProvider }.returns(javaCompileProvider)
 
         val result = androidPluginDataProvider.getJavaTargetCompatibilityVersion()
 
@@ -101,7 +100,34 @@ class AndroidPluginDataProviderTest : BaseMockable() {
 
     @Test
     fun `Get variant path`() {
-        fail("Not implemented")
+        val variant = createAndSetMainVariantMock()
+        val flavorName = "someFlavorName"
+        val androidVariantPathResolver = mockk<AndroidVariantPathResolver>()
+        val buildType = mockk<BuildType>()
+        val buildTypeName = "someBuildTypeName"
+        val flavor1 = mockk<ProductFlavor>()
+        val flavor2 = mockk<ProductFlavor>()
+        val flavorName1 = "someFlavor"
+        val flavorName2 = "anotherFlavor"
+        val flavors = listOf(flavor1, flavor2)
+        val expectedPath = listOf("some", "path")
+        every { flavor1.name }.returns(flavorName1)
+        every { flavor2.name }.returns(flavorName2)
+        every { variant.flavorName }.returns(flavorName)
+        every { variant.buildType }.returns(buildType)
+        every { buildType.name }.returns(buildTypeName)
+        every { variant.productFlavors }.returns(flavors)
+        every {
+            androidVariantPathResolverFactory.create(
+                variantName, flavorName, buildTypeName,
+                listOf(flavorName1, flavorName2)
+            )
+        }.returns(androidVariantPathResolver)
+        every { androidVariantPathResolver.getTopBottomPath() }.returns(expectedPath)
+
+        val result = androidPluginDataProvider.getVariantPath()
+
+        Truth.assertThat(result).isEqualTo(expectedPath)
     }
 
     private fun getVariantsIterator(
@@ -123,5 +149,13 @@ class AndroidPluginDataProviderTest : BaseMockable() {
         val mock = mockk<ApplicationVariant>()
         every { mock.name }.returns(name)
         return mock
+    }
+
+    private fun createAndSetMainVariantMock(vararg otherVariantNames: String): ApplicationVariant {
+        val appVariants = mockk<DomainObjectSet<ApplicationVariant>>()
+        val variant = createApplicationVariantMock(variantName)
+        every { androidAppExtension.applicationVariants }.returns(appVariants)
+        every { appVariants.iterator() }.returns(getVariantsIterator(variant, *otherVariantNames))
+        return variant
     }
 }
