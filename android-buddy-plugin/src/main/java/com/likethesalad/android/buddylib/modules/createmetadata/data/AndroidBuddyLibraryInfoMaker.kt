968e01a44e863d03168237d4eb38f0875d025824
Cesar <likethesalad@gmail.com>
2021-02-23 18:52:16 +0000

    Adding validations when creating AndroidLibraryInfo

--- android-buddy-plugin/src/main/java/com/likethesalad/android/buddylib/modules/createmetadata/data/AndroidBuddyLibraryInfoMaker.kt
+++ android-buddy-plugin/src/main/java/com/likethesalad/android/buddylib/modules/createmetadata/data/AndroidBuddyLibraryInfoMaker.kt
@@ -10,18 +10,28 @@ class AndroidBuddyLibraryInfoMaker @Inject constructor(private val projectInfoPr
 
     companion object {
         private val VALID_ID_REGEX = Regex("[a-z]+[a-z0-9]*([.-]?[a-z0-9]+)*")
+        private val VALID_GROUP_REGEX = Regex("[^\\s\\t\\n]+")
+        private val CHECK_NAME_WRAPPING = Regex("(^[^a-zA-Z0-9]+|[^a-zA-Z0-9]+\$)")
+        private val CLEAN_NAME_SPACING = Regex("[\\s\\t\\n]+")
     }
 
     fun make(
         id: String,
         pluginNames: Set<String>
     ): AndroidBuddyLibraryInfo {
+        val group = projectInfoProvider.getGroup()
+        val name = projectInfoProvider.getName()
+        val version = projectInfoProvider.getVersion()
+
         validateId(id)
+        validateGroup(group)
+        validateName(name)
+
         return AndroidBuddyLibraryInfo(
             id,
-            projectInfoProvider.getGroup(),
-            projectInfoProvider.getName(),
-            projectInfoProvider.getVersion(),
+            group,
+            cleanName(name),
+            version,
             pluginNames
         )
     }
@@ -36,6 +46,23 @@ class AndroidBuddyLibraryInfoMaker @Inject constructor(private val projectInfoPr
                         "\n -There cannot be a consecutive '-' or '.' after a '-' and/or '.'."
             )
         }
+    }
+
+    private fun validateGroup(group: String) {
+        if (!VALID_GROUP_REGEX.matches(group)) {
+            throw IllegalArgumentException("The group provided '$group' has not a valid format")
+        }
+    }
+
+    private fun validateName(name: String) {
+        if (name.trim().isEmpty()) {
+            throw IllegalArgumentException("The name provided '$name' is not valid, it cannot be empty")
+        }
+    }
+
+    private fun cleanName(name: String): String {
+        val nameProperlyWrapped = name.replace(CHECK_NAME_WRAPPING, "")
+        return nameProperlyWrapped.replace(CLEAN_NAME_SPACING, "_").toLowerCase()
     }
 
 }
--- android-buddy-plugin/src/test/java/com/likethesalad/android/buddylib/modules/createmetadata/data/AndroidBuddyLibraryInfoMakerTest.kt
+++ android-buddy-plugin/src/test/java/com/likethesalad/android/buddylib/modules/createmetadata/data/AndroidBuddyLibraryInfoMakerTest.kt
@@ -47,6 +47,8 @@ class AndroidBuddyLibraryInfoMakerTest : BaseMockable() {
         checkIdIsValid("one_id-something", false)
         checkIdIsValid("one--id-something", false)
         checkIdIsValid("one..id-something", false)
+        checkIdIsValid("one.-id-something", false)
+        checkIdIsValid("one-.id-something", false)
         checkIdIsValid("one id", false)
         checkIdIsValid("one   id", false)
         checkIdIsValid("one@id", false)
@@ -54,6 +56,42 @@ class AndroidBuddyLibraryInfoMakerTest : BaseMockable() {
         checkIdIsValid("one.id-", false)
         checkIdIsValid("  one.id", false)
         checkIdIsValid("", false)
+    }
+
+    @Test
+    fun `Check if group is valid`() {
+        checkGroupIsValid("some.group", true)
+        checkGroupIsValid("some group", false)
+        checkGroupIsValid("", false)
+    }
+
+    @Test
+    fun `Check if name is valid`() {
+        checkNameIsValid("some.name", true)
+        checkNameIsValid("some name", true)
+        checkNameIsValid("Some name", true)
+        checkNameIsValid("    ", false)
+        checkNameIsValid("", false)
+    }
+
+    @Test
+    fun `Format name if needed`() {
+        checkNameFormatted("some.name", "some.name")
+        checkNameFormatted("some-name", "some-name")
+        checkNameFormatted("some name", "some_name")
+        checkNameFormatted("somename", "somename")
+        checkNameFormatted("Some name", "some_name")
+        checkNameFormatted("Some name  ", "some_name")
+        checkNameFormatted("Some  name  ", "some_name")
+        checkNameFormatted(" Some  name  ", "some_name")
+        checkNameFormatted(" Some  name", "some_name")
+        checkNameFormatted(" Some name  ", "some_name")
+        checkNameFormatted("-some name", "some_name")
+        checkNameFormatted("-.somename", "somename")
+        checkNameFormatted("somename--", "somename")
+        checkNameFormatted("some name-", "some_name")
+        checkNameFormatted("some name.", "some_name")
+        checkNameFormatted("some name-", "some_name")
     }
 
     private fun checkIdIsValid(id: String, shouldBeValid: Boolean) {
@@ -71,12 +109,63 @@ class AndroidBuddyLibraryInfoMakerTest : BaseMockable() {
             if (shouldBeValid) {
                 throw e
             }
-            Truth.assertThat(e.message).isEqualTo("Id provided '$id' has not a valid format:" +
-                    "\n -Only lowercase letters, numbers, '.' and '-' are allowed." +
-                    "\n -It must start with a letter." +
-                    "\n -It cannot end neither with '-' nor with '.'." +
-                    "\n -There cannot be a consecutive '-' or '.' after a '-' and/or '.'.")
+            Truth.assertThat(e.message).isEqualTo(
+                "Id provided '$id' has not a valid format:" +
+                        "\n -Only lowercase letters, numbers, '.' and '-' are allowed." +
+                        "\n -It must start with a letter." +
+                        "\n -It cannot end neither with '-' nor with '.'." +
+                        "\n -There cannot be a consecutive '-' or '.' after a '-' and/or '.'."
+            )
         }
+    }
+
+    private fun checkGroupIsValid(group: String, shouldBeValid: Boolean) {
+        val id = "some.id"
+        val name = "some.name"
+        val version = "1.0.0"
+        val pluginNames = setOf("some.Plugin")
+
+        try {
+            callMake(id, group, name, version, pluginNames)
+            if (!shouldBeValid) {
+                fail("The group '$group' should not be valid")
+            }
+        } catch (e: IllegalArgumentException) {
+            if (shouldBeValid) {
+                throw e
+            }
+            Truth.assertThat(e.message).isEqualTo("The group provided '$group' has not a valid format")
+        }
+    }
+
+    private fun checkNameIsValid(name: String, shouldBeValid: Boolean) {
+        val id = "some.id"
+        val group = "some.group"
+        val version = "1.0.0"
+        val pluginNames = setOf("some.Plugin")
+
+        try {
+            callMake(id, group, name, version, pluginNames)
+            if (!shouldBeValid) {
+                fail("The name '$name' should not be valid")
+            }
+        } catch (e: IllegalArgumentException) {
+            if (shouldBeValid) {
+                throw e
+            }
+            Truth.assertThat(e.message).isEqualTo("The name provided '$name' is not valid, it cannot be empty")
+        }
+    }
+
+    private fun checkNameFormatted(originalName: String, expectedName: String) {
+        val id = "some.id"
+        val group = "some.group"
+        val version = "1.0.0"
+        val pluginNames = setOf("some.Plugin")
+
+        val info = callMake(id, group, originalName, version, pluginNames)
+
+        Truth.assertThat(info.name).isEqualTo(expectedName)
     }
 
     private fun callMake(
