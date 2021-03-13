e8275baa5def8f231e92b0fec04fa4a4159ac97c
Cesar <likethesalad@gmail.com>
2021-03-13 21:02:12 +0000

    Updated app extension

--- android-buddy-plugin/src/main/java/com/likethesalad/android/buddy/extension/AndroidBuddyExtension.kt
+++ android-buddy-plugin/src/main/java/com/likethesalad/android/buddy/extension/AndroidBuddyExtension.kt
@@ -1,19 +1,15 @@
 package com.likethesalad.android.buddy.extension
 
 import com.likethesalad.android.buddy.extension.libraries.LibrariesOptions
+import org.gradle.api.Action
 import org.gradle.api.model.ObjectFactory
-import org.gradle.api.provider.Provider
 
 @Suppress("UnstableApiUsage")
 open class AndroidBuddyExtension(objectFactory: ObjectFactory) {
 
     val librariesOptions = objectFactory.newInstance(LibrariesOptions::class.java)
 
-    @JvmOverloads
-    fun librariesPolicy(policyNameProvider: Provider<String>, argsProvider: Provider<Iterable<Any>>? = null) {
-        librariesOptions.policyName.set(policyNameProvider)
-        if (argsProvider != null) {
-            librariesOptions.args.set(argsProvider)
-        }
+    fun librariesPolicy(action: Action<LibrariesOptions>) {
+        action.execute(librariesOptions)
     }
 }
