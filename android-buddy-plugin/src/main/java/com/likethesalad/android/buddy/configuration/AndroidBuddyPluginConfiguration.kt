9712bce6735eeced1d232ad5818598f12081d2d6
Cesar <likethesalad@gmail.com>
2021-06-28 18:55:48 +0100

    Using lazy for all configs

--- android-buddy-plugin/src/main/java/com/likethesalad/android/buddy/configuration/AndroidBuddyPluginConfiguration.kt
+++ android-buddy-plugin/src/main/java/com/likethesalad/android/buddy/configuration/AndroidBuddyPluginConfiguration.kt
@@ -18,22 +18,30 @@ class AndroidBuddyPluginConfiguration
     private val lazyLibrariesScope: LibrariesScope by lazy {
         librariesScopeMapper.librariesScopeExtensionToLibrariesScope(extension.librariesPolicy.scope)
     }
-
-    private val transformationScope by lazy { extension.transformationScope }
+    private val lazyTransformationScope: MutableSet<in QualifiedContent.Scope> by lazy {
+        val scope = extension.transformationScope.scope.getOrElse(TransformationScopeType.PROJECT.name)
+        when (scope.toUpperCase()) {
+            TransformationScopeType.ALL.name -> mutableSetOf(
+                QualifiedContent.Scope.PROJECT,
+                QualifiedContent.Scope.SUB_PROJECTS,
+                QualifiedContent.Scope.EXTERNAL_LIBRARIES
+            )
+            else -> mutableSetOf(QualifiedContent.Scope.PROJECT)
+        }
+    }
+    private val lazyExcludePrefixes: Set<String> by lazy {
+        extension.transformationScope.excludePrefixes.getOrElse(emptySet())
+    }
 
     fun getLibrariesScope(): LibrariesScope {
         return lazyLibrariesScope
     }
 
     fun getTransformationScope(): MutableSet<in QualifiedContent.Scope> {
-        val scope = transformationScope.scope.getOrElse(TransformationScopeType.PROJECT.name)
-        return when (scope.toUpperCase()) {
-            TransformationScopeType.ALL.name -> mutableSetOf(QualifiedContent.Scope.PROJECT, QualifiedContent.Scope.SUB_PROJECTS, QualifiedContent.Scope.EXTERNAL_LIBRARIES)
-            else -> mutableSetOf(QualifiedContent.Scope.PROJECT)
-        }
+        return lazyTransformationScope
     }
 
     fun getExcludePrefixes(): Set<String> {
-        return transformationScope.excludePrefixes.getOrElse(emptySet())
+        return lazyExcludePrefixes
     }
 }
